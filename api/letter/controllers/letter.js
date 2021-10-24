3a512bf0f713e592c323819f7f80215fc711a537
peddn <public@peddn.net>
2021-10-24 18:51:18 +0200

    basic development

--- api/letter/controllers/letter.js
+++ api/letter/controllers/letter.js
@@ -5,23 +5,40 @@
 
 'use strict';
 
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
 
-    create: async (ctx) => {
-        let result = await strapi.query('user').find();
-        console.log(result);
-
-        let entity;
-        if (ctx.is('multipart')) {
-          const { data, files } = parseMultipartData(ctx);
-          entity = await strapi.services.letter.create(data, { files });
-        } else {
-          entity = await strapi.services.letter.create(ctx.request.body);
-        }
-        console.log(entity);
-        return sanitizeEntity(entity, { model: strapi.models.letter });
+  async create(ctx) {
+    console.log('FINALLY');
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.letter.create(data, { files });
+    } else {
+      entity = await strapi.services.letter.create(ctx.request.body);
     }
+
+    console.log(entity)
+
+
+    const users = await strapi.query('user', 'users-permissions').find();
+    for(const user of users) {
+      const role = user.role.name;
+      if(role === 'teacher' || role === 'principal') {
+        await strapi.services.perusal.create({
+          signed: false,
+          date: new Date(),
+          owner: user.id,
+          letter: entity.id
+        })
+      }
+    }
+
+
+
+
+    return sanitizeEntity(entity, { model: strapi.models.letter });
+  },
 
 };
